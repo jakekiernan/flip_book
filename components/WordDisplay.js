849bf58238f9d29363e0438156d7d7c1dffa4b7e
Jake Kiernan <kiernan.jake@gmail.com>
2019-09-14 22:38:16 -0500

    feat: add time interval to cycle through words

--- components/WordDisplay.js
+++ components/WordDisplay.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {parse} from '../utils/parse';
+import {splitArr, showWord, useInterval} from '../utils/parse';
 import {stage} from '../assets/text/Shakespeare';
 
 export default function WordDisplay() {
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(count + 1);
+  }, 200);
+
   return (
     <View style={styles.container}>
-      <Text>{parse(stage)}</Text>
+      <Text
+        style={styles.baseText}>
+        {showWord(splitArr(stage), count)}
+      </Text>
     </View>
   );
 }
@@ -17,5 +26,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  baseText: {
+    fontSize: 50,
   },
 });
--- utils/parse.js
+++ utils/parse.js
@@ -1,5 +1,29 @@
-const parse = text => {
+import { useEffect, useRef } from 'react';
+
+const splitArr = text => {
   return text.split(" ");
 }
 
-export {parse};
+const showWord = (arr, index) => {
+  return arr[index];
+}
+
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export {splitArr, showWord, useInterval};
